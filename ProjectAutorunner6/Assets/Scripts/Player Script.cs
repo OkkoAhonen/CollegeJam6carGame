7cc0036853e8f41b8bd4431a5acd1f1324f469d1
Trollipopper <113014080+Trollipopper@users.noreply.github.com>
2024-03-21 12:35:03 +0200

    Elmon kamaa
    Player ja Gamemanager, sex

---
+++ ProjectAutorunner6/Assets/Scripts/Player Script.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float accelerationSpeed, maxSpeed, turnpower, carHealth, currentSpeed;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        Drive();
+    }
+
+    void Drive()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * turnpower / (1 + currentSpeed * 0.15f)); //K‰‰ntyy hitaammin, mit‰ nopeamin menee
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -turnpower / (1 + currentSpeed * 0.15f));
+        }
+
+        transform.position -= new Vector3(0, transform.rotation.z + 0.00001f * -currentSpeed * transform.rotation.z, 0);
+
+    }
+}
