b37d20d9ccf67413f5d1a129fb1eaded3003d120
Trollipopper <113014080+Trollipopper@users.noreply.github.com>
2024-03-22 09:51:58 +0200

    Elmon roinaaa
    kaikkee hienoo, ei toimi kaikki vielä

---
+++ ProjectAutorunner6/Assets/Scripts/EnemyCarScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCarScript : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
