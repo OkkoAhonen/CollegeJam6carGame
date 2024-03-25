64283c01d61d38d3b8b29c4ad3dadd0e2d4608b3
Trollipopper <113014080+Trollipopper@users.noreply.github.com>
2024-03-25 12:16:31 +0200

    Elmon kamaa
    paljon kaikkee haista vi

--- ProjectAutorunner6/Assets/Scripts/EnemyCarScript.cs
+++ ProjectAutorunner6/Assets/Scripts/EnemyCarScript.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using UnityEngine;
 
 public class EnemyCarScript : MonoBehaviour
 {
+    public float enemySpeed, stopSpeed;
+    private bool hadInside;
 
+    public LayerMask checkedLayers; // Mitkä objektit tarkistaa
+    
+    Rigidbody2D rb;
 
-    void Start()
+    private AudioManager audioManager;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = 0;
+
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    public void SetDestination(GameObject path)
+    {
+        transform.position = path.transform.GetChild(1).transform.position;
+
+        Vector3 direction = path.transform.GetChild(2).transform.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+        rb.velocity = direction * enemySpeed;
     }
 
     void Update()
     {
-        
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position + transform.up * 2.3f, new Vector2(1, 2), transform.rotation.z, checkedLayers);
+
+        if (colliders.Length > 0 )
+        {
+            if (!hadInside)
+            {
+                hadInside = true;
+                rb.drag = stopSpeed;
+                audioManager.Play("Honk");
+            }
+            foreach (Collider2D collider in colliders)
+            {
+                //Debug.Log("Object inside trigger: " + collider.name);
+            }
+        }
+        else
+        {
+            if (hadInside)
+            {
+                hadInside = false;
+                rb.drag = 0;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = UnityEngine.Color.magenta;
+        Gizmos.DrawWireCube(transform.position + transform.up * 2.3f, new Vector2(1, 2));
     }
 }
--- ProjectAutorunner6/Assets/Scripts/GameManager.cs
+++ ProjectAutorunner6/Assets/Scripts/GameManager.cs
@@ -4,16 +4,20 @@ using System.IO;
 using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GameManager : MonoBehaviour
 {
-    public float enemySpeed, minSpawnTime, maxSpawnTime, timeEqualizer, timeEqualizerChangeMultiplier;
+    public float minSpawnTime, maxSpawnTime, timeEqualizer, timeEqualizerChangeMultiplier;
     public GameObject carPaths;
     public GameObject[] enemies;
-    public List<GameObject> paths = new();
+    private List<GameObject> paths = new();
+
+    private GameObject gameHolder;
 
     void Start()
     {
+        gameHolder = GameObject.Find("GameHolder");
         foreach (Transform transPath in carPaths.transform)
         {
             GameObject path = transPath.gameObject;
@@ -39,27 +43,19 @@ public class GameManager : MonoBehaviour
         List<GameObject> pathsInThisWave;
         while (true)
         {
-            waveSize = Random.Range(1, 3);
-            pathsInThisWave = paths;
-            waveSize--;
+            waveSize = Random.Range(2, 4);
+            pathsInThisWave = new List<GameObject>(paths);
 
             while (waveSize != 0)
             {
-                GameObject enemy = Instantiate(enemies[Random.Range(0, enemies.Length - 1)]); //Spawns random enemy from enemies
+                waveSize--;
+                GameObject enemy = Instantiate(enemies[Random.Range(0, enemies.Length - 1)], gameHolder.transform); //Spawns random enemy from enemies
 
                 int childIndex = pathsInThisWave.Count;
                 GameObject path = pathsInThisWave[Random.Range(0, childIndex)]; //Chooses which path cars take
-                pathsInThisWave.Remove(path); //Jostain syystä poistaa paths listasta myös!!
-                foreach (GameObject p in pathsInThisWave)
-                {
-                    Debug.Log(p);
-                }
-
-                enemy.transform.position = path.transform.GetChild(1).transform.position;
-                Debug.Log(path.transform.GetChild(1).name + " " + path.transform.GetChild(1).transform.position);
-                Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
-                Vector3 direction = (path.transform.GetChild(2).transform.position - transform.position);
-                rb.velocity = direction * enemySpeed;
+                pathsInThisWave.Remove(path);
+                EnemyCarScript enemyScript = enemy.GetComponent<EnemyCarScript>();
+                enemyScript.SetDestination(path);
             }
 
             randomizedTime = timeEqualizer > 0
